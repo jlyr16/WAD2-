098ad8e93dd929c7146cf4515e41966778278ec3
jlyr16 <111680658+jlyr16@users.noreply.github.com>
2022-10-12 22:36:52 +0800

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+    <img
+      alt="Vue logo"
+      src="https://cdn-icons-png.flaticon.com/512/3136/3136101.png"
+    />
     <HelloWorld msg="hello!!!!!!" />
   </div>
 </template>
