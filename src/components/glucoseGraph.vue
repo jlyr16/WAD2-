176f3c59e7e1af89fad2852a0f963fc837545c5f
jlyr16 <111680658+jlyr16@users.noreply.github.com>
2022-10-18 00:02:02 +0800

    2 files modified

--- src/components/glucoseGraph.vue
+++ src/components/glucoseGraph.vue
@@ -1,33 +1,61 @@
 <template>
   <div class="chart">
-    <line-chart :data="chartData" :options="chartOptions" />
+    <canvas id="myChart" width="400" height="400"></canvas>
   </div>
 
   
 </template>
 
 <script>
+import Chart from 'chart.js';
+
 export default {
   name: 'glucoseGraph',
-  data() {
-    return {
-      chartData: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-          {
-            label: 'Data One',
-            backgroundColor: '#f87979',
-            data: [40, 39, 10, 40, 39, 80, 40],
-          },
-        ],
-      },
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-      },
-    };
+  props:{
+    msg: String,
   },
+  mounted() {
+   const ctx= document.getElementById('myChart');
+
+   const myChart = new Chart(ctx,{
+
+    type: 'line',
+    data:{
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets:[
+        {
+          label: 'Glucose Level',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }
+      ]
+
+    }
+   })
+   myChart;
+
+  }
+
 };
+
+ 
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 const { createApp } = require('vue');
-import Vue from 'vue'
 import App from "./App.vue";
-import Chartkick from 'vue-chartkick';
-import Chart from 'chart.js';
+
 
 createApp(App).mount("#app");
-Vue.use(Chartkick.use(Chart));
+
