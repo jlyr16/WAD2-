e7815f8e0e6a81b102419f3fb085d90d3c22ccf6
jlyr16 <111680658+jlyr16@users.noreply.github.com>
2022-10-25 12:18:19 +0800

    1 file modified, 1 added

---
+++ src/components/input.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="form">
+    <form @submit="addData" method="post">
+      <input type="text" v-model="input" placeholder="Enter glucose level" />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'post',
+  data() {
+    return {
+      glucoseLevel: [],
+    };
+  },
+  methods: {
+    submit() {
+      axios
+        .post('https://api.airtable.com/v0/app7prXyz92FkQR63/Furniture', {
+          glucoseLevel: this.glucoseLevel,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
